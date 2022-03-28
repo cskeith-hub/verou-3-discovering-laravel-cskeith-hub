f20552832c24bbcc4e5382ed2142c28037db6a16
unknown <cskeith01@gmail.com>
2022-03-28 22:23:13 +0200

    Add post allowens of A-z and _\-

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 use App\Http\Controllers\Controller;
-Route::get('/', [Controller::class, 'message']);
-Route::get('/{post}', [Controller::class, 'nextPage']);
+Route::get('/', [Controller::class, 'message'])->where('/', '[A-z_\-]+'); // where("#", '[A-z]+') now only read Upper & lowerCase letters + underScore and Dash
+Route::get('/{post}', [Controller::class, 'nextPage'])->where('post', '[A-z]+');
 
 
 
